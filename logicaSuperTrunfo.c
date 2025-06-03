3dd5b1b300dec502139aefd18b5e72af85d3cb13
Rodrigo Nonaka <rodrigoncg@gmail.com>
2025-06-03 10:16:00 -0300

    desativa antiga lógica de comparação e implementa nova lógica em switch

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,90 +1,128 @@
 #include <stdio.h>
 
 int main(){
-    //banco de variaveis
-        //variaveis carta 1
-        char estado1[5], codigo1[5], cidade1[50];
-        int pontoTur1;
-        unsigned long int populacao1;
-        float area1, pib1, densidadePop1, pibConversao1, pibCapita1, superPoder1;
-
-        //variaveis carta 2
-        char estado2[5], codigo2[5], cidade2[50];
-        int pontoTur2;
-        unsigned long int populacao2;
-        float area2, pib2, densidadePop2, pibConversao2, pibCapita2, superPoder2;
-
-        //comparativos
-        int compPopulacao = 0;
-        int compArea = 0;
-        int compPib = 0;
-        int compPontoTur = 0;
-        int compDensidade = 0;
-        int compPibCapita = 0;
-        int compSuperPoder = 0;
-
+    printf("\n\n"); //quebra de linha inicial
 
     //leitura dos dados das cartas
-        //carta 1
-        printf("Insira os dados da primeira carta \n");
-        printf("Digite a sigla do estado: \n");
-        scanf(" %s", estado1);
-        printf("Digite o código da carta: \n");
-        scanf(" %s", codigo1);
-        printf("Digite o nome da cidade: \n");
-        scanf(" %49[^\n]", cidade1);
-        printf("Digite o número da população: \n");
-        scanf(" %d", &populacao1);
-        printf("Digite a área (em km²): \n");
-        scanf(" %f", &area1);
-        printf("Digite o PIB: \n");
-        scanf(" %f", &pib1);
-        printf("Digite o número de pontos turísticos: \n");
-        scanf(" %d", &pontoTur1);
+        //variaveis carta 1
+            char estado1[5], codigo1[5], cidade1[50];
+            int pontoTur1;
+            unsigned long int populacao1;
+            float area1, pib1, densidadePop1, pibConversao1, pibCapita1, superPoder1;
 
-        //carta 2
-        printf("Insira os dados da segunda carta \n");
-        printf("Digite a sigla do estado: \n");
-        scanf(" %s", estado2);
-        printf("Digite o código da carta: \n");
-        scanf(" %s", codigo2);
-        printf("Digite o nome da cidade: \n");
-        scanf(" %49[^\n]", cidade2);
-        printf("Digite o número da população: \n");
-        scanf(" %d", &populacao2);
-        printf("Digite a área (em km²): \n");
-        scanf(" %f", &area2);
-        printf("Digite o PIB: \n");
-        scanf(" %f", &pib2);
-        printf("Digite o número de pontos turísticos: \n");
-        scanf(" %d", &pontoTur2);
+        //variaveis carta 2
+            char estado2[5], codigo2[5], cidade2[50];
+            int pontoTur2;
+            unsigned long int populacao2;
+            float area2, pib2, densidadePop2, pibConversao2, pibCapita2, superPoder2;
 
-    //calculo das variaveis das cartas
+        //entrada dos dados da carta 1
+            printf("Insira os dados da primeira carta \n");
+            printf("Digite a sigla do estado: \n");
+            scanf(" %s", estado1);
+            printf("Digite o código da carta: \n");
+            scanf(" %s", codigo1);
+            printf("Digite o nome da cidade: \n");
+            scanf(" %49[^\n]", cidade1);
+            printf("Digite o número da população: \n");
+            scanf(" %d", &populacao1);
+            printf("Digite a área (em km²): \n");
+            scanf(" %f", &area1);
+            printf("Digite o PIB: \n");
+            scanf(" %f", &pib1);
+            printf("Digite o número de pontos turísticos: \n");
+            scanf(" %d", &pontoTur1);
+
+        //entrada dos dados da carta 2
+            printf("Insira os dados da segunda carta \n");
+            printf("Digite a sigla do estado: \n");
+            scanf(" %s", estado2);
+            printf("Digite o código da carta: \n");
+            scanf(" %s", codigo2);
+            printf("Digite o nome da cidade: \n");
+            scanf(" %49[^\n]", cidade2);
+            printf("Digite o número da população: \n");
+            scanf(" %d", &populacao2);
+            printf("Digite a área (em km²): \n");
+            scanf(" %f", &area2);
+            printf("Digite o PIB: \n");
+            scanf(" %f", &pib2);
+            printf("Digite o número de pontos turísticos: \n");
+            scanf(" %d", &pontoTur2);
+
+    //calculo das variaveis compostas das cartas
         //calculo de variaveis da carta 1
-        densidadePop1 = (float)((float)populacao1 / area1);
-        pibConversao1 = (pib1 * 1000000000);
-        pibCapita1 = (float)(pibConversao1 / populacao1);
-        superPoder1 = populacao1 + pib1 + pontoTur1 + pibCapita1 + (-densidadePop1);
-
+            densidadePop1 = (float)((float)populacao1 / area1);
+            pibConversao1 = (pib1 * 1000000000);
+            pibCapita1 = (float)(pibConversao1 / populacao1);
+            superPoder1 = populacao1 + pib1 + pontoTur1 + pibCapita1 + (-densidadePop1);
         //calculo de variaveis da carta 2
-        densidadePop2 = (float)((float)populacao2 / area2);
-        pibConversao2 = (pib2 * 1000000000);
-        pibCapita2 = (float)(pibConversao2 / populacao2);
-        superPoder2 = populacao2 + pib2 + pontoTur2 + pibCapita2 + (-densidadePop2);  
+            densidadePop2 = (float)((float)populacao2 / area2);
+            pibConversao2 = (pib2 * 1000000000);
+            pibCapita2 = (float)(pibConversao2 / populacao2);
+            superPoder2 = populacao2 + pib2 + pontoTur2 + pibCapita2 + (-densidadePop2);  
         
 
+    //menu de escolha da comparação
+        //variaveis do menu 
+            int escolhaMenu;
+        //    int compPopulacao = 0;
+        //    int compArea = 0;
+        //    int compPib = 0;
+        //    int compPontoTur = 0;
+        //    int compDensidade = 0;
+        //    int compPibCapita = 0;
+        //    int compSuperPoder = 0;
+
+        //menu interativo
+            printf("\n"); //quebra de linha
+            printf("Escolha qual atributo você quer comparar:\n");
+            printf("1. População;\n");
+            printf("2. Área;\n");
+            printf("3. PIB;\n");
+            printf("4. Pontos Turísticos;\n");
+            printf("5. Densidade Demográfica;\n");
+            printf("6. PIB per Capita\n");
+            printf("7. Super Poder\n");
+            printf("Escolha: ");
+            scanf("%d", &escolhaMenu);
+
+        //switch menu interativo
+            switch (escolhaMenu){
+            case 1:
+                if(populacao1 > populacao2){
+                    printf("A cidade vencedora com maior população é: %s\n", cidade1);
+                }else if(populacao1 < populacao2){
+                    printf("A cidade vencedora com maior população é: %s\n", cidade2);
+                }else{
+                    printf("Empate, as duas cidades têm populações iguais.\n");
+                }
+                break;
+            case 2:
+                /* code */
+                break;
+            case 3:
+                /* code */
+                break;
+            case 4:
+                /* code */
+                break;
+            case 5:
+                /* code */
+                break;
+            case 6:
+                /* code */
+                break;
+            case 7:
+                /* code */
+                break;
+            default:
+                printf("Opção inválida, tente novamente.\n");
+                break;
+            }
+
+            /*
     //logica de comparacao entre cartas
-        printf("\n"); //quebra de linha
-        printf("Comparação entre os atributos das cartas:\n");
-        //comparacao de populacao
-        if(populacao1 > populacao2){
-            printf("%s tem maior população.\n", cidade1);
-            compPopulacao = 1;
-        }else{
-            printf("%s tem maior população.\n", cidade2);
-            compPopulacao = 2;
-        }
- 
         //comparacao de area
         if(area1 > area2){
             printf("%s tem maior área.\n", cidade1);
@@ -191,6 +229,6 @@ int main(){
         }else{
             printf("A cidade vencedora com maior super poder é: %s\n", cidade2);
         }
-
+            */
     return 0;
 }
